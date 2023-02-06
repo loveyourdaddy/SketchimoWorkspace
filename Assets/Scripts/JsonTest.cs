c06041b5841db36af90eec93491187f3ff95ceb6
Inseo <isjang08@kaist.ac.kr>
2023-02-06 22:11:10 +0900

    collision frame in json in unity -> python

--- Assets/Scripts/JsonTest.cs
+++ Assets/Scripts/JsonTest.cs
@@ -6,19 +6,23 @@ using UnityEngine;
 
 namespace Sketchimo.Models
 {
-    // JsonMotion for serializable
+    // Class to save JsonMotion 
     [System.Serializable]
     public class JsonMotion
     {
         public string characterName;
         public string motionName;
         public int totalFrame;
+        public int[] collisionFrame;
         public int numberofVertex;
         public float fps = 60;
         public Quaternion[] rotation;
         public Vector3[] position;
         public Vector3[] vertices;
-        public int[] vertexIndicesArray;
+        public int[] vertexIndicesArray0; // left hand indices 
+        public int[] vertexIndicesArray1; // right hand indices 
+        // public List: smallest distance의 pair vertex indices는 python에서 찾자. 
+        // 여기서 해도 되는데, python에서 어차피 vertex position을 들고 있어야해. 
     }
 
     public class JsonTest : MonoBehaviour
@@ -27,75 +31,22 @@ namespace Sketchimo.Models
         public GameObject man;
         private SkinnedMeshRenderer skin;
         private Mesh mesh;
-        private JsonMotion jsonMotion;
+        [HideInInspector]
+        public JsonMotion jsonMotion;
         private bool isUpdated = false;
         public BoxCollider box;
-        // public int vertexIndices[];
-        public List<int> vertexIndices;
-        public int[] vertexIndicesArray;
+        public int[] vertexIndicesArray0;
+        public int[] vertexIndicesArray1;
         void Awake()
         {
-            // Get aabb and vertex indices 
-            // GetVertexIndices();
-
-            // ONLY CHANGE THIS
             SaveJson();
             // UpdateMotionFromJson();
         }
 
-        void printRecursive(Transform currBone)
-        {
-            for (int i = 0; i < currBone.childCount; i++)
-            {
-                Debug.Log(currBone.name);
-                Debug.Log(currBone.transform.position.ToString("F4"));
-                var childBone = currBone.GetChild(i);
-                printRecursive(childBone);
-                Debug.Log(i);
-            }
-        }
-
-        // void Start()
-        // {
-
-        // }
-        public void GetVertexIndices()
-        {
-
-            // Get aabb bound
-            Vector3 maxBound = box.bounds.max;
-            Vector3 minBound = box.bounds.min;
-
-            // Get vertices in bound
-            Transform ybot = man.transform.GetChild(0);
-            SkinnedMeshRenderer skin = ybot.GetComponentInChildren<SkinnedMeshRenderer>();
-
-            // mesh in T pose 
-            Mesh mesh = skin.sharedMesh;
-            // mesh.vertices;
-
-            // check it is T pose 
-            // Transform root = ybot.Find("mixamorig:Hips");
-            // printRecursive(root);
-
-            // get vertices indices 
-            // bound안에 있는 index을 리스트 
-
-            for (int i = 0; i < mesh.vertexCount; i++)
-            {
-                if (maxBound.x > mesh.vertices[i].x && minBound.x < mesh.vertices[i].x
-                    && maxBound.y > mesh.vertices[i].y && minBound.y < mesh.vertices[i].y
-                    && maxBound.z > mesh.vertices[i].z && minBound.z < mesh.vertices[i].z)
-                {
-                    vertexIndices.Add(i);
-                }
-            }
-
-            vertexIndicesArray = vertexIndices.ToArray();
-        }
-
+        // unity -> python
         public void SaveJson()
         {
+            // update motion data to json
             var refMotion = motionInfo.refMotion;
             int numPose = refMotion.data.Count;
             int numJoint = refMotion.data[0].joints.Length;
@@ -120,88 +71,30 @@ namespace Sketchimo.Models
                 }
             }
 
-            // // save mesh information 
-            // skin = man.transform.GetComponentInChildren<SkinnedMeshRenderer>();
-            // mesh = skin.sharedMesh;
-            // jsonMotion.numberofVertex = mesh.vertexCount;
-            // jsonMotion.vertices = new Vector3[mesh.vertexCount * motionInfo.GetTotalFrame()];
-            // jsonMotion.vertexIndicesArray = vertexIndicesArray;
-
-            // // Set motion start 
-            // GetComponent<Controllers.MotionController>().SetPlayState(0);
+            // Set motion start 
+            GetComponent<Controllers.MotionController>().SetPlayState(0);
         }
 
         public void Update()
         {
-            if(GetComponent<Controllers.MotionController>().CurrentPlay == 0)
+            // Save json when animation finish
+            // unity -> python
+            if (GetComponent<Controllers.MotionController>().CurrentPlay == 0 &&
+                motionInfo.GetCurrentFrame() >= motionInfo.GetTotalFrame() - 1)
             {
-            //     int count = mesh.vertexCount;
-            //     int numberofVertex = mesh.vertexCount;
-                Debug.Log("frame: " + motionInfo.GetCurrentFrame());
-                // for (int i = 0; i < count; i++)
-                // {
-                //     jsonMotion.vertices[numberofVertex * (motionInfo.GetCurrentFrame()) + i] = mesh.vertices[i]; // - 1
-                // }
+                Debug.Log("Total frame: " + motionInfo.GetTotalFrame().ToString());
+                Debug.Log(motionInfo.GetCurrentFrame().ToString());
 
-                if(motionInfo.GetCurrentFrame() >= motionInfo.GetTotalFrame() - 1) // && isUpdated == false
-                {
-                    Debug.Log("Total frame: " + motionInfo.GetTotalFrame().ToString());
-                    Debug.Log(motionInfo.GetCurrentFrame().ToString());
+                // set stop
+                GetComponent<Controllers.MotionController>().SetPlayState(1);
 
-                    // set stop
-                    GetComponent<Controllers.MotionController>().SetPlayState(1);
-
-                    // save json
-                    string jsonFile = JsonUtility.ToJson(jsonMotion);
-                    File.WriteAllText(Application.dataPath + "/Json/UnityOutput_clapping.json", jsonFile); // UnityOutput_Tpose
-                    // isUpdated = true;
-                }
+                // save json
+                string jsonFile = JsonUtility.ToJson(jsonMotion);
+                File.WriteAllText(Application.dataPath + "/Json/UnityOutput_clapping.json", jsonFile); // UnityOutput_Tpose
             }
-
-            // find hand joint 
-            // Transform hip = man.transform.GetChild(0).transform.GetChild(2); // LeftHand
-            // var leftHand = hip.Find("mixamorig:Spine/mixamorig:Spine1/mixamorig:Spine2/mixamorig:LeftShoulder/mixamorig:LeftArm/mixamorig:LeftForeArm/mixamorig:LeftHand");
-            // // var tmmmp = leftHand.gameObject.GetComponent<>;
-
-            // Vector3 center = leftHand.transform.position;
-            // Vector3 extents = Vector3.one;
-
-            // //find position of last vertex index 
-            // // 가장 오른쪽에 있는 vertex index을 찾아보자. -> python
-            // Transform ybot_trf = man.transform.GetChild(0);
-            // Transform alpha_surface_trf = ybot_trf.GetChild(0);
-            // var render = alpha_surface_trf.GetComponent<SkinnedMeshRenderer>();
-            // var mesh = alpha_surface_trf.GetComponent<Mesh>();
-
-            // // find min x 
-            // var verteices = mesh.vertices;
-            // float min_x = 10;
-            // int min_index = 0;
-            // for(int i=0; i<mesh.vertexCount; i++)
-            // {
-            //     if(min_x < verteices[i].x)
-            //     {
-            //         min_x = verteices[i].x;
-            //         min_index = i;
-            //     }
-            // }
         }
 
-        void OnDrawGizmos()
-        {
-            // bounds.center = center;
-
-
-            // find vertices index
-
-
-            // Bounds bounds = render.bounds;
-            // Gizmos.matrix = Matrix4x4.identity;
-            // Gizmos.color = Color.blue;
-            // Gizmos.DrawWireCube(bounds.center, bounds.extents*2);
-        }
-
-        // Update unity motion (motionInfo) from python
+        // python -> unity
         public void UpdateMotionFromJson()
         {
             // parse json 
--- Assets/Scripts/collsiion.cs
+++ Assets/Scripts/collsiion.cs
@@ -1,40 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sketchimo.Models;
 
 public class collsiion : MonoBehaviour
 {
     public BoxCollider box;
+    public JsonTest jsonTest;
+    [HideInInspector]
+    public List<int> vertexIndices;
+    public List<int> collisionFrames;
+
     // Start is called before the first frame update
     void Start()
     {
-        // var box = GetComponent<BoxCollider>();
-        // Vector3 center = box.center;
-        // Vector3 size = box.size;
+        box = GetComponent<BoxCollider>();
+        // 각 collision box 안의 vertex index을 가지고 있자. 
+        GetVertexIndices();
+    }
+    // Get aabb and vertex indices 
+    
+    public void GetVertexIndices()
+    {
 
-        // Vector3 maxBound = box.bounds.max;
-        // Vector3 minBound = box.bounds.min;
+        // Get aabb bound
+        Vector3 maxBound = box.bounds.max;
+        Vector3 minBound = box.bounds.min;
 
-        //find vertex index 
+        // Get vertices in bound
+        Transform ybot = jsonTest.man.transform.GetChild(0);
+        SkinnedMeshRenderer skin = ybot.GetComponentInChildren<SkinnedMeshRenderer>();
 
-        
+        // mesh in T pose 
+        Mesh mesh = skin.sharedMesh;
+
+        // get vertices indices: bound안에 있는 index을 리스트 
+        for (int i = 0; i < mesh.vertexCount; i++)
+        {
+            if (maxBound.x > mesh.vertices[i].x && minBound.x < mesh.vertices[i].x
+                && maxBound.y > mesh.vertices[i].y && minBound.y < mesh.vertices[i].y
+                && maxBound.z > mesh.vertices[i].z && minBound.z < mesh.vertices[i].z)
+            {
+                vertexIndices.Add(i);
+            }
+        }
+        if(box.transform.name == "mixamorig:LeftHand")
+            jsonTest.jsonMotion.vertexIndicesArray0 = vertexIndices.ToArray();
+        else // RightHane
+            jsonTest.jsonMotion.vertexIndicesArray1 = vertexIndices.ToArray();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Debug.Log("start");
-    }
 
-    void OnCollisionEnter(Collision collision)
-    {
-        var a = collision.contacts;
-        Debug.Log("collision detection");
     }
 
     void OnTriggerEnter(Collider collider)
     {
         // var a = collision.contacts;
-        Debug.Log("collision detection");
+        var motionInfo = jsonTest.motionInfo;
+        int collisionFrame = motionInfo.GetCurrentFrame();
+        Debug.Log("collision detection at " + collisionFrame.ToString());
+
+        // jsonFile.jsonMotion.frame 저장        
+        collisionFrames.Add(collisionFrame);
+
+        // // run at last time 
+        // if(motionInfo.GetCurrentFrame() >= motionInfo.GetTotalFrame() - 1)
+        jsonTest.jsonMotion.collisionFrame = collisionFrames.ToArray();
+
+        // vertex pair 저장.
+        // collision이 발생했을 때, vertex indices 사이에 가장 가까운 vertex을 찾자. 
+    }
+
+    void OnApplicatoinQuit()
+    {
+        jsonTest.jsonMotion.collisionFrame = collisionFrames.ToArray();
     }
 }
